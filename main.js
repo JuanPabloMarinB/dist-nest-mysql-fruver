3639bfd9f84a5bd6554fb385e223368579286e77
Juan Pablo Marin Buitrago <111198348+JuanPabloMarinB@users.noreply.github.com>
2023-07-05 23:04:43 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,15 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./modules/app.module");
 const cors = require('cors');
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, {
+        
+        options: {
+            cors: true,
+            
+            host: '3.234.171.120',
+        },
+    });
     app.use(cors());
     await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
-//# sourceMappingURL=main.js.map
