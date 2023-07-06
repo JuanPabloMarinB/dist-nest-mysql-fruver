64f467e8f5603616ad079b645550b8097ece02ae
Juan Pablo Marin Buitrago <111198348+JuanPabloMarinB@users.noreply.github.com>
2023-07-05 23:51:47 -0400

    Update main.js

--- main.js
+++ main.js
@@ -13,6 +13,6 @@ async function bootstrap() {
         },
     });
     app.use(cors());
-    await app.listen(process.env.PORT || 3000, process.env.IP);
+    await app.listen(process.env.PORT || 3000, '0.0.0.0'); 
 }
 bootstrap();
